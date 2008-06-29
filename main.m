56058d1575baaca57bbfccfc663e78ce650b7fd0
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-29 11:23:04 +0100

    Initial project creation

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 29/06/2008.
+//  Copyright ManicPanda.com 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
