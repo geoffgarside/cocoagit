da5c3e4b0f61e858dceb9cd59ce34fae42f88cf0
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-29 11:37:55 +0100

    Add note about where the hashing and compression functions were sourced

--- NSData+Hashing.h
+++ NSData+Hashing.h
@@ -5,6 +5,9 @@
 //  Created by Geoffrey Garside on 29/06/2008.
 //  Copyright 2008 ManicPanda.com. All rights reserved.
 //
+//  Methods extracted from source given at
+//  http://www.cocoadev.com/index.pl?NSDataCategory
+//
 
 #import <Cocoa/Cocoa.h>
 
