7ddaa52c8ca5cd0c3c37ce6e6646f4eee5db332f
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-29 17:37:26 +0100

    Add GITBlob class

---
+++ GITBlob.h
@@ -0,0 +1,16 @@
+//
+//  GITBlob.h
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 29/06/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "GITObject.h"
+
+@interface GITBlob : GITObject {
+    
+}
+
+@end
---
+++ GITBlob.m
@@ -0,0 +1,14 @@
+//
+//  GITBlob.m
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 29/06/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import "GITBlob.h"
+
+
+@implementation GITBlob
+
+@end
