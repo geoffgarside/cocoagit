25d7971bcad658e9827e3ed3450389b08f200150
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-29 11:27:48 +0100

    Add NSData Compression category additions

---
+++ NSData+Compression.h
@@ -0,0 +1,24 @@
+//
+//  NSData+Compression.h
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 29/06/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSData (Compression)
+
+#pragma mark -
+#pragma mark Zlib Compression routines
+- (NSData *) zlibInflate;
+- (NSData *) zlibDeflate;
+
+#pragma mark -
+#pragma mark Gzip Compression routines
+- (NSData *) gzipInflate;
+- (NSData *) gzipDeflate;
+
+@end
