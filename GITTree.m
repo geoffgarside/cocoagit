0d02a346f01753b48701467b0ed5705d0eb10fbe
Geoff Garside <geoff@geoffgarside.co.uk>
2008-06-30 17:20:45 +0100

    Add SHA1 pack/unpack methods for handling Git Tree object entries

--- GITTree.m
+++ GITTree.m
@@ -8,7 +8,62 @@
 
 #import "GITTree.h"
 
+static const char hexchars[] = "0123456789abcdef";
+
+@interface GITTree (Private)
+
+- (NSData*)packSHA1:(NSString*)unpackedSHA1;
+- (NSString*)unpackSHA1FromString:(NSString*)packedSHA1;
+- (NSString*)unpackSHA1FromData:(NSData*)packedSHA1;
+
+@end
+
 
 @implementation GITTree
 
 @end
+
+@implementation GITTree (Private)
+
+- (NSData*)packSHA1:(NSString*)unpackedSHA1
+{
+    unsigned int highBits, lowBits, bits;
+    NSMutableData *packedSHA1 = [NSMutableData dataWithCapacity:20];
+    for (int i = 0; i < [unpackedSHA1 length]; i++)
+    {
+        if (i % 2 == 0) {
+            highBits = (strchr(hexchars, [unpackedSHA1 characterAtIndex:i]) - hexchars) << 4;
+        } else {
+            lowBits = strchr(hexchars, [unpackedSHA1 characterAtIndex:i]) - hexchars;
+            bits = (highBits | lowBits);
+            [packedSHA1 appendBytes:&bits length:1];
+        }
+    }
+    return packedSHA1;
+}
+- (NSString*)unpackSHA1FromString:(NSString*)packedSHA1
+{
+    NSMutableString *unpackedSHA1 = [NSMutableString stringWithCapacity:40];
+    for(int i = 0; i < 20; i++)
+    {
+        unsigned int bits = [packedSHA1 characterAtIndex:i];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits >> 4]];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits & 0xf]];
+    }
+    return unpackedSHA1;
+}
+- (NSString*)unpackSHA1FromData:(NSData*)packedSHA1
+{
+    NSMutableString *unpackedSHA1 = [NSMutableString stringWithCapacity:40];
+    for(int i = 0; i < 20; i++)
+    {
+        unsigned int bits;
+        [packedSHA1 getBytes:&bits range:NSMakeRange(i, 1)];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits >> 4]];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits & 0xf]];
+    }
+    return unpackedSHA1;
+}
+
+@end
+
