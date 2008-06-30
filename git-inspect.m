125f6d8a90af7c1462363b94481f185c95cac28c
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 00:52:29 +0100

    Add start of command line argument processing

--- git-inspect.m
+++ git-inspect.m
@@ -5,6 +5,13 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     
+    if (argc != 2) {
+        NSLog(@"Usage: %s sha1hash", argv[0]);
+        exit 0;
+    }
+    
+    NSString *inspectHash = [NSString stringWithCString:argv[1]];
+    
     [pool drain];
     return 0;
 }
