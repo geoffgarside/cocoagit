31bb78425a17313fbe3bd540130c0f80c0843bba
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 00:44:58 +0100

    Add git-inspect command line application

---
+++ git-inspect.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import "NSData+Compression.h"
+#import "NSData+Hashing.h"
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    [pool drain];
+    return 0;
+}
