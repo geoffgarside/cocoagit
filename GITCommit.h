9322cca043b92ed03293e8432eb958079ac7ca4e
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 19:53:59 +0100

    Add fields and methods for GIT Commit

--- GITCommit.h
+++ GITCommit.h
@@ -11,8 +11,37 @@
 
 extern const NSString *kGITObjectCommitType;
 
-@interface GITCommit : GITObject {
+@class GITTree, GITUser
 
+@interface GITCommit : GITObject {
+    GITCommit * parent;
+    GITTree * tree;
+    
+    GITUser * author;
+    GITUser * committer;
+    
+    NSCalendarDate * authoredAt;
+    NSCalendarDate * committedAt;
+    
+    NSString * message;
 }
+
+#pragma mark -
+#pragma mark Properties
+@property(retain) GITCommit * parent;
+@property(retain) GITTree * tree;
+
+@property(retain) GITUser * author;
+@property(retain) GITUser * committer;
+@property(retain) NSCalendarDate * authoredAt;
+@property(retain) NSCalendarDate * committedAt;
+
+@property(retain) NSString * message;
+
+#pragma mark -
+#pragma mark Instance Methods
+- (void)setAuthor:(GITUser*)user withDate:(NSCalendarDate*)theDate;
+- (void)setCommitter:(GITUser*)user withDate:(NSCalendarDate*)theDate;
+- (NSString*)formattedUser:(GITUser*)theUser withDate:(NSCalendarDate*)theDate;
 
 @end
--- GITCommit.m
+++ GITCommit.m
@@ -7,9 +7,39 @@
 //
 
 #import "GITCommit.h"
+#import "GITTree.h"
+#import "GITUser.h"
 
 const NSString *kGITObjectCommitType = @"commit";
 
 @implementation GITCommit
+
+#pragma mark -
+#pragma mark Properties
+@synthesize parent;
+@synthesize tree;
+@synthesize author;
+@synthesize committer;
+@synthesize authoredAt;
+@synthesize committedAt;
+@synthesize message;
+
+#pragma mark -
+#pragma mark Instance Methods
+- (void)setAuthor:(GITUser*)user withDate:(NSCalendarDate*)theDate
+{
+    self.author = user;
+    self.authoredAt = theDate;
+}
+- (void)setCommitter:(GITUser*)user withDate:(NSCalendarDate*)theDate
+{
+    self.committer = user;
+    self.committedAt = theDate;
+}
+- (NSString*)formattedUser:(GITUser*)theUser withDate:(NSCalendarDate*)theDate
+{
+    return [NSString stringWithFormat:@"%@ %d %@", theUser,
+            [theDate timeIntervalSince1970], [theDate descriptionWithCalendarFormat:@"%z"]];
+}
 
 @end
