ca3944768e8617b2b078f55d5ee8ef855c03b2d8
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 15:04:13 +0100

    Add GITTag class

---
+++ GITTag.h
@@ -0,0 +1,18 @@
+//
+//  GITTag.h
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 01/07/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "GITObject.h"
+
+extern const NSString *kGITObjectTagType;
+
+@interface GITTag : GITObject {
+
+}
+
+@end
---
+++ GITTag.m
@@ -0,0 +1,15 @@
+//
+//  GITTag.m
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 01/07/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import "GITTag.h"
+
+const NSString *kGITObjectTagType = @"tag";
+
+@implementation GITTag
+
+@end
