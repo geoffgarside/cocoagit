c42ec045d90523d36ec9011da37d39f4d078a8a3
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 14:54:07 +0100

    Add GITObject to list of sources for git-inspect tool

--- git-inspect.m
+++ git-inspect.m
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import "NSData+Compression.h"
 #import "NSData+Hashing.h"
+#import "GITObject.h"
 
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
