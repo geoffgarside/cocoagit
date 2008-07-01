bc1f8741941e3a8136f40cb9b99a492171686214
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-01 15:03:00 +0100

    Add GITCommit class

---
+++ GITCommit.h
@@ -0,0 +1,18 @@
+//
+//  GITCommit.h
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 01/07/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "GITObject.h"
+
+extern const NSString *kGITObjectCommitType;
+
+@interface GITCommit : GITObject {
+
+}
+
+@end
---
+++ GITCommit.m
@@ -0,0 +1,15 @@
+//
+//  GITCommit.m
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 01/07/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import "GITCommit.h"
+
+const NSString *kGITObjectCommitType = @"commit";
+
+@implementation GITCommit
+
+@end
