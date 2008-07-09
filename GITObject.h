288329aca3fae827814caf50e919152bf554f4f2
Geoff Garside <geoff@geoffgarside.co.uk>
2008-07-09 21:57:56 +0100

    Add size to GITObjects

--- GITObject.h
+++ GITObject.h
@@ -11,7 +11,8 @@
 extern const NSString * kGITObjectsDirectoryRoot;
 
 @interface GITObject : NSObject {
-    NSString* sha1;
+    NSString* sha1;     //!< Stores the SHA1 of objects committed to the file system
+    NSUInteger size;   //!< Stores the content size of the data portion of the object
 }
 
 #pragma mark -
