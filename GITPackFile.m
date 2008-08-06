e5ff9cff08f00bef9c4d1f00c984d7a18101f6cf
Geoff Garside <geoff@geoffgarside.co.uk>
2008-08-06 10:58:00 +0100

    Extend reading idx files further, specifically the Fan Out

--- GITPackFile.m
+++ GITPackFile.m
@@ -13,7 +13,10 @@ const NSRange kGITPackFileSignatureRange = {     0,      4 };
 const NSRange kGITPackFileVersionRange   = {     4,      4 };
 const NSRange kGITPackFileNumberRange    = {     8,      4 };
 
-const NSUInteger kGITPackIndexFanOutSize = 256;
+const NSUInteger kGITPackIndexFanOutSize  = 4;      // bytes
+const NSUInteger kGITPackIndexFanOutCount = 256;
+const NSUInteger kGITPackIndexFanOutEnd   = kGITPackIndexFanOutSize * kGITPackIndexFanOutCount;
+const NSUInteger kGITPackIndexEntrySize   = 24;     // bytes
 
 @interface GITPackFile ()
 @property(readwrite,retain) GITRepo * repo;
@@ -95,8 +98,12 @@ const NSUInteger kGITPackIndexFanOutSize = 256;
                                           options:NSUncachedRead
                                             error:&err];
     unichar buf[4];
-    NSUInteger i;
-    
+    NSUInteger i, lastCount, thisCount;
+    NSMutableArray * offsets = [NSMutableArray arrayWithCapacity:256];
+
+    lastCount = thisCount = 0;
+    [offsets addObject:[NSNumber numberWithUnsignedInteger:0]];  // Fill in \0 char count
+
     // The Fanout table
     // The fanout table consists of 256 entries. These
     // entries correspond to first byte of an object
@@ -110,11 +117,28 @@ const NSUInteger kGITPackIndexFanOutSize = 256;
     // At the moment I am not sure if the fan out
     // entry index corresponds to the compressed or
     // uncompressed SHA1 hash value.
-    for (i = 0; i < kGITPackIndexFanOutSize; i++)
+    for (i = 0; i < kGITPackIndexFanOutCount; i++)
     {
-        [idx getBytes:buf range:NSMakeRange(i * 4, 4)];
-        
+        [idx getBytes:buf range:NSMakeRange(i * kGITPackIndexFanOutSize, kGITPackIndexFanOutSize)];
+        thisCount = [self integerFromBytes:buf length:kGITPackIndexFanOutSize];
+
+        // Assuming an NSUInteger is less expensive than calling
+        // [[idxOffsets objectAtIndex:i - 1] unsignedIntegerValue]
+        if (lastCount > thisCount)
+        {
+            NSString * reason = [NSString stringWithFormat:@"IDX %@ is corrupt", [self.idxPath lastPathComponent]];
+            NSException * ex  = [NSException exceptionWithName:@"GITPackFileCorruptIndexException"
+                                                        reason:reason
+                                                      userInfo:nil];
+            @throw ex;
+        }
+
+        [offsets addObject:[NSNumber numberWithUnsignedInteger:thisCount]];
+        lastCount = thisCount;
     }
+
+    self.numberOfObjects = thisCount;   // The value of the last offset
+    self.idxOffsets = offsets;
 }
 - (NSUInteger)integerFromBytes:(unichar*)bytes length:(NSUInteger)length
 {
@@ -122,6 +146,30 @@ const NSUInteger kGITPackIndexFanOutSize = 256;
     for (i = 0; i <= length; i++)
         value = (value << 4) | bytes[i];
     return value;
+}
+- (NSUInteger)offsetForSha1:(NSString*)sha1
+{
+    unichar firstByte = [sha1 characterAtIndex:0];
+
+    // startOffset: Take the count of objects starting with bytes < :firstByte
+    // endOffset: Take the count of objects starting with bytes <= :firstByte
+    NSUInteger startOffset = [self.idxOffsets objectAtIndex:firstByte - 1];
+    NSUInteger endOffset   = [self.idxOffsets objectAtIndex:firstByte];
+
+    NSRange searchRange = NSMakeRange(startOffset * kGITPackIndexEntrySize + kGITPackIndexFanOutEnd,
+                                      (endOffset - startOffset) * kGITPackIndexEntrySize);
+    // This is probably wrong
+    if (range.length < 1)
+    {
+        NSString * reason = [NSString stringWithFormat:@"SHA1 %@ is not known in this PACK %@",
+                             sha1, [self.idxPath lastPathComponent]];
+        NSException * ex = [NSException exceptionWithName:@"GITPackFileUnknownSHA1"
+                                                   reason:reason
+                                                 userInfo:nil];
+        @throw ex;
+    }
+
+    // Now we need to loop through the
 }
 
 @end
