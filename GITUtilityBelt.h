fc6a10efbfc29982a02798db2571d1288af01544
Geoff Garside <geoff@geoffgarside.co.uk>
2008-08-12 11:07:07 +0100

    Suit up with our new Utility Belt full of useful methods we'll probably need all over the place.

---
+++ GITUtilityBelt.h
@@ -0,0 +1,14 @@
+//
+//  GITUtilityBelt.h
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 12/08/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NSData   * packSHA1(NSString * unpackedSHA1);
+NSString * unpackSHA1FromString(NSString * packedSHA1);
+NSString * unpackSHA1FromData(NSData * packedSHA1);
+NSUInteger integerFromBytes(unichar * bytes, NSUInteger length);
---
+++ GITUtilityBelt.m
@@ -0,0 +1,67 @@
+//
+//  GITUtilityBelt.m
+//  CocoaGit
+//
+//  Created by Geoffrey Garside on 12/08/2008.
+//  Copyright 2008 ManicPanda.com. All rights reserved.
+//
+
+#import "GITUtilityBelt.h"
+
+const NSUInteger kGITPackedSha1Length   = 20;
+const NSUInteger kGITUnpackedSha1Length = 40;
+
+NSData *
+packSHA1(NSString * unpackedSHA1)
+{
+    unsigned int highBits, lowBits, bits;
+    NSMutableData *packedSHA1 = [NSMutableData dataWithCapacity:kGITPackedSha1Length];
+    for (int i = 0; i < [unpackedSHA1 length]; i++)
+    {
+        if (i % 2 == 0) {
+            highBits = (strchr(hexchars, [unpackedSHA1 characterAtIndex:i]) - hexchars) << 4;
+        } else {
+            lowBits = strchr(hexchars, [unpackedSHA1 characterAtIndex:i]) - hexchars;
+            bits = (highBits | lowBits);
+            [packedSHA1 appendBytes:&bits length:1];
+        }
+    }
+    return packedSHA1;
+}
+
+NSString *
+unpackSHA1FromString(NSString * packedSHA1)
+{
+    unsigned int bits;
+    NSMutableString *unpackedSHA1 = [NSMutableString stringWithCapacity:kGITUnpackedSha1Length];
+    for(int i = 0; i < kGITPackedSha1Length; i++)
+    {
+        bits = [packedSHA1 characterAtIndex:i];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits >> 4]];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits & 0xf]];
+    }
+    return unpackedSHA1;
+}
+
+NSString *
+unpackSHA1FromData(NSData * packedSHA1)
+{
+    unsigned int bits;
+    NSMutableString *unpackedSHA1 = [NSMutableString stringWithCapacity:kGITUnpackedSha1Length];
+    for(int i = 0; i < kGITPackedSha1Length; i++)
+    {
+        [packedSHA1 getBytes:&bits range:NSMakeRange(i, 1)];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits >> 4]];
+        [unpackedSHA1 appendFormat:@"%c", hexchars[bits & 0xf]];
+    }
+    return unpackedSHA1;
+}
+
+NSUInteger
+integerFromBytes(unichar * bytes, NSUInteger length)
+{
+    NSUInteger i, value = 0;
+    for (i = 0; i <= length; i++)
+        value = (value << 8) | bytes[i];
+    return value;
+}
