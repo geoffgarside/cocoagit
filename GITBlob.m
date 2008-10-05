abeeac0be62a674042095fe870a10e39e64c9b13
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-05 16:49:07 +0100

    Stop shadowing variables with arguments

--- GITBlob.m
+++ GITBlob.m
@@ -25,15 +25,15 @@
 @synthesize data;
 
 - (id)initWithHash:(NSString*)hash
-           andData:(NSData*)data
-          fromRepo:(GITRepo*)repo
+           andData:(NSData*)blobData
+          fromRepo:(GITRepo*)parentRepo
 {
     if (self = [super init])
     {
-        self.repo = repo;
+        self.repo = parentRepo;
         self.sha1 = hash;
-        self.size = [data length];
-        self.data = data;
+        self.size = [blobData length];
+        self.data = blobData;
     }
     return self;
 }
