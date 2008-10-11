cfbff56f230cdfb394c5cfd68eea25851166501e
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-11 20:53:08 +0100

    Add some documentation on GITBlob

--- GITBlob.h
+++ GITBlob.h
@@ -12,17 +12,24 @@
 @class GITRepo;
 @interface GITBlob : NSObject <GITObject>
 {
-    GITRepo  * repo;
-    NSString * sha1;
-    NSUInteger size;
-    NSData   * data;
+    GITRepo  * repo;    //!< Repository which this blob is a part of
+    NSString * sha1;    //!< The SHA1 hash reference of this blob
+    NSUInteger size;    //!< The file size of the contents of this blob
+    NSData   * data;    //!< The binary data of this blob
 }
 
 @property(readonly,copy) NSString * sha1;
 @property(readonly,assign) NSUInteger size;
 @property(readonly,copy) NSData * data;
 
+/*!
+ Indicates if the blobs contents are likely to be textual.
+*/
 - (BOOL)canBeRepresentedAsString;
+
+/*!
+ Converts the binary data into a string.
+*/
 - (NSString*)stringValue;
 
 @end
