9b7860e55312e086c964da364613a4c79ccb5008
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-12 02:17:37 +0100

    Add documentation to the GITRepo class

--- GITRepo.h
+++ GITRepo.h
@@ -9,19 +9,32 @@
 #import <Cocoa/Cocoa.h>
 
 @class GITBranch, GITTag;
+/*! A repository of git objects.
+ * This class serves to encapsulate the access to the
+ * objects of a repository.
+ */
 @interface GITRepo : NSObject <NSCopying>
 {
-    NSString * root;
-    NSString * desc;    // Interesting issue here
-                        // the function used for
-                        // an object to print itself
-                        // is -description
+    NSString * root;    //!< Path to the repository root
+    NSString * desc;    //!< Description of the repository
+                        // Interesting issue here the function used for
+                        // an object to print itself is -description
 }
 
 @property(readonly,copy) NSString * root;
 @property(readonly,copy) NSString * desc;
 
+/*! Creates and returns a repo object with the provided root.
+ * \param repoRoot The path, relative or absolute, to the repository root.
+ * \return A repo object with the provided root.
+ */
 - (id)initWithRoot:(NSString*)repoRoot;
+
+/*! Creates and returns a repo object with the provided root.
+ * \param repoRoot The path, relative or absolute, to the repository root.
+ * \param isBare Flag indicating if the repository is bare.
+ * \return A repo object with the provided root.
+ */
 - (id)initWithRoot:(NSString*)repoRoot bare:(BOOL)isBare;
 
 /*
