0d4b395dfce3f64ae99ae5fe9dcaf1a214faa226
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-13 09:38:06 +0100

    Add documentation for tree entry

--- GITTreeEntry.h
+++ GITTreeEntry.h
@@ -14,12 +14,12 @@ extern const NSUInteger kGITUnpackedSha1Length;
 @class GITRepo;
 @interface GITTreeEntry : NSObject
 {
-    GITRepo  * repo;
-    NSString * name;
-    NSUInteger mode;
+    GITRepo  * repo;    //!< Repository the entry belongs to. Used for accessing the object
+    NSString * name;    //!< Name of the entry, either a file or directory name
+    NSUInteger mode;    //!< File mode of the entry
     
-    NSString * sha1;
-    id object;
+    NSString * sha1;    //!< SHA1 of the object referenced
+    id object;          //!< The object which is referenced. This is lazy loaded.
 }
 
 @property(readonly,copy) GITRepo * repo;
@@ -28,8 +28,27 @@ extern const NSUInteger kGITUnpackedSha1Length;
 @property(readonly,copy) NSString * sha1;
 @property(readonly,copy) id object;
 
-- (id)initWithTreeLine:(NSString*)treeLine repo:(GITRepo*)theRepo;
-- (id)initWithMode:(NSUInteger)mode name:(NSString*)name hash:(NSString*)hash repo:(GITRepo*)theRepo;
-- (id)initWithModeString:(NSString*)mode name:(NSString*)name hash:(NSString*)hash repo:(GITRepo*)theRepo;
+/*! Creates and returns a new entry by extracting the information tree line.
+ * \param treeLine The raw line as extracted from a tree object file
+ * \param repo The repository the object belongs to
+ * \return A new entry
+ */
+- (id)initWithTreeLine:(NSString*)treeLine repo:(GITRepo*)repo;
+
+/*! Creates and returns a new entry the given settings
+ * \param mode The file mode of the file or directory described
+ * \param name The file name of the filr or directory described
+ * \param hash The SHA1 of the object referenced
+ * \param repo The repository the object referenced belongs to
+ */
+- (id)initWithMode:(NSUInteger)mode name:(NSString*)name hash:(NSString*)hash repo:(GITRepo*)repo;
+
+/*! Creates and returns a new entry the given settings
+ * \param mode The file mode as a string of the file or directory described
+ * \param name The file name of the filr or directory described
+ * \param hash The SHA1 of the object referenced
+ * \param repo The repository the object referenced belongs to
+ */
+- (id)initWithModeString:(NSString*)mode name:(NSString*)name hash:(NSString*)hash repo:(GITRepo*)repo;
 
 @end
