0741fd4ab878e69638523f2c8c119072b189206c
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-14 14:27:40 +0100

    Update GITCommit tests

--- GITCommitTests.h
+++ GITCommitTests.h
@@ -13,21 +13,17 @@
     GITRepo   * repo;
     GITCommit * commit;
     NSString  * commitSHA1;
-    NSUInteger rawCommitSize;
 }
 
 @property(readwrite,retain) GITRepo   * repo;
 @property(readwrite,retain) GITCommit * commit;
 @property(readwrite,copy)   NSString  * commitSHA1;
-@property(readwrite,assign) NSUInteger rawCommitSize;
 
 - (void)testIsNotNil;
 - (void)testSha1MatchesInitialSha1;
-- (void)testSizeMatchesRawSize;
 - (void)testAuthorIsNotNil;
-
-#pragma mark -
-#pragma mark Helpers
-- (NSData*)rawCommitData;
+- (void)testAuthoredIsNotNil;
+- (void)testCommitterIsNotNil;
+- (void)testCommittedIsNotNil;
 
 @end
--- GITCommitTests.m
+++ GITCommitTests.m
@@ -9,46 +9,26 @@
 #import "GITCommitTests.h"
 #import "GITCommit.h"
 #import "GITRepo.h"
-#import "GITRepo.h"
-#import "NSData+Compression.h"
 
 @implementation GITCommitTests
 @synthesize repo;
 @synthesize commit;
 @synthesize commitSHA1;
-@synthesize rawCommitSize;
 
 - (void)setUp
 {
     [super setUp];
     self.repo = [[GITRepo alloc] initWithRoot:@"."];
     self.commitSHA1 = @"bc1f8741941e3a8136f40cb9b99a492171686214";
-    self.commit = [[GITCommit alloc] initWithHash:commitSHA1 andData:[self rawCommitData] fromRepo:repo];
+    self.commit = [repo commitWithSha1:commitSHA1];
 }
 - (void)tearDown
 {
     self.repo = nil;
     self.commitSHA1 = nil;
-    self.rawCommitSize = 0;
+    self.commit = nil;
     [super tearDown];
 }
-- (NSData*)rawCommitData
-{
-    NSString * objectType;
-    NSUInteger objectSize;
-    NSData * objectData;
-    
-    NSData * rawCommit = [[NSData dataWithContentsOfFile:[repo objectPathFromHash:commitSHA1]] zlibInflate];
-    
-    [repo extractFromData:rawCommit
-                     type:&objectType 
-                     size:&objectSize
-                  andData:&objectData];
-    
-    self.rawCommitSize = objectSize;
-    return objectData;
-}
-
 - (void)testIsNotNil
 {
     STAssertNotNil(commit, nil);
@@ -56,10 +36,6 @@
 - (void)testSha1MatchesInitialSha1
 {
     STAssertEqualObjects(commit.sha1, commitSHA1, nil);
-}
-- (void)testSizeMatchesRawSize
-{
-    STAssertEquals(commit.size, rawCommitSize, @"Sizes should be equal");
 }
 - (void)testAuthorIsNotNil
 {
