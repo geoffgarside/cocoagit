7b731e9071b09adedb44d097a3ae7c04488e345c
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-19 14:09:23 +0100

    Add documentation of the category methods on NSTimeZone

--- NSTimeZone+Offset.h
+++ NSTimeZone+Offset.h
@@ -10,6 +10,17 @@
 
 @interface NSTimeZone (Offset)
 
+/*! Creates and returns a time zone with the specified offset.
+ * The string is broken down into the hour and minute components
+ * which are then used to work out the number of seconds from GMT.
+ * \param offset The timezone offset as a string such as "+0100"
+ * \return A time zone with the specified offset
+ * \see +timeZoneForSecondsFromGMT:
+ */
 + (id)timeZoneWithStringOffset:(NSString*)offset;
+
+/*! Returns the receivers offset as an HHMM formatted string.
+ * \return The receivers offset as a string in HHMM format.
+ */
 - (NSString*)offsetString;
 @end
