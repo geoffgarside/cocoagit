990223dcb47ff1986fd663a5dc8a0b9dfb92de2e
Geoff Garside <geoff@geoffgarside.co.uk>
2008-10-19 14:24:23 +0100

    Add documentation for Hashing and Searching NSData categories

--- NSData+Hashing.h
+++ NSData+Hashing.h
@@ -15,7 +15,14 @@
 
 #pragma mark -
 #pragma mark SHA1 Hashing routines
+/*! Returns the SHA1 digest of the receivers contents.
+ * \return SHA1 digest of the receivers contents.
+ */
 - (NSData*) sha1Digest;
+
+/*! Returns a string with the SHA1 digest of the receivers contents.
+ * \return String with the SHA1 digest of the receivers contents.
+ */
 - (NSString*) sha1DigestString;
 
 @end
--- NSData+Searching.h
+++ NSData+Searching.h
@@ -10,8 +10,30 @@
 
 @interface NSData (Searching)
 
+/*! Returns the range of bytes up to the first NULL byte from start.
+ * \return Range of bytes up to the first NULL from start. If no NULL
+ * can be found then the NSRange.location will be set to NSNotFound.
+ * \see memchr
+ */
 - (NSRange)rangeOfNullTerminatedBytesFrom:(NSInteger)start;
-- (NSData*)subdataFromIndex:(NSUInteger)anIndex;
-- (NSData*)subdataToIndex:(NSUInteger)anIndex;
+
+/*! Returns a data object containing a copy of the receiver's bytes
+ * that fall within the limits specified by a given index and the
+ * end of the bytes.
+ * \param index Start of the range which defines the limits to extract.
+ * \return A data object containing a copy of the receiver's bytes
+ * that fall within the limits of index and the end of the bytes.
+ * \see -subdataWithRange:
+ */
+- (NSData*)subdataFromIndex:(NSUInteger)index;
+
+/*! Returns a data object containing a copy of the receiver's bytes
+ * that fall within the first byte and index.
+ * \param index End of the range which defines the limits to extract.
+ * \return A data object containing a copy of the receiver's bytes
+ * that fall within the first byte and index.
+ * \see -subdataWithRange:
+ */
+- (NSData*)subdataToIndex:(NSUInteger)index;
 
 @end
