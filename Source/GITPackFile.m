3e6f0643ba9dd02b9ea247f244ff718c486b7a94
Geoff Garside <geoff@geoffgarside.co.uk>
2008-11-04 22:56:28 +0000

    Stupid Geffy, always check crap you copy from another class

--- Source/GITPackFile.m
+++ Source/GITPackFile.m
@@ -13,14 +13,14 @@
 #pragma mark Class Cluster Alloc Methods
 + (id)alloc
 {
-    if ([self isEqual:[GITPackIndex class]])
-        return [GITPlaceholderPackIndex alloc];
+    if ([self isEqual:[GITPackFile class]])
+        return [GITPlaceholderPackFile alloc];
     else return [super alloc];
 }
 + (id)allocWithZone:(NSZone*)zone
 {
-    if ([self isEqual:[GITPackIndex class]])
-        return [GITPlaceholderPackIndex allocWithZone:zone];
+    if ([self isEqual:[GITPackFile class]])
+        return [GITPlaceholderPackFile allocWithZone:zone];
     else return [super allocWithZone:zone];
 }
 @end
