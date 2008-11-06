df95f3d3c6d7f0ca7db20f7e542dfc8a8cb32862
Geoff Garside <geoff@geoffgarside.co.uk>
2008-11-06 00:30:35 +0000

    Tidy the source

--- Source/GITPackIndexVersion2.m
+++ Source/GITPackIndexVersion2.m
@@ -67,21 +67,6 @@ static const NSUInteger kGITPackIndexEntrySize   = 24;         //!< bytes
     NSMutableArray * _offsets = [NSMutableArray arrayWithCapacity:256];
 
     lastCount = thisCount = 0;
-
-    // The Fanout table
-    // The fanout table consists of 256 entries. These
-    // entries correspond to the first byte of an object
-    // sha1. The value at each entry is the number of
-    // object sha1's in the corresponding PACK file
-    // in which the first byte is less than or equal
-    // to the entry index. This also applies to the
-    // list of SHA1 entries in the IDX file as well.
-    // The value of each entry should be greater than
-    // or equal to the previous value, if it is not
-    // then the index file is corrupt.
-    // At the moment I am not sure if the fan out
-    // entry index corresponds to the compressed or
-    // uncompressed SHA1 hash value.
     for (i = 0; i < kGITPackIndexFanOutCount; i++)
     {
         [self.data getBytes:buf range:NSMakeRange((i * kGITPackIndexFanOutSize) + kGITPackIndexFanOutStart, kGITPackIndexFanOutSize)];
