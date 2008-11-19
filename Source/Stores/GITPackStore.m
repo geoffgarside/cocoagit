3b289a87602e57839605512df2cf54edf7fbb077
Geoff Garside <geoff@geoffgarside.co.uk>
2008-11-19 00:18:44 +0000

    Add loading of pack files from .git/objects/pack

--- Source/Stores/GITPackStore.m
+++ Source/Stores/GITPackStore.m
@@ -12,6 +12,8 @@
 @interface GITPackStore ()
 @property(readwrite,copy) NSString * packsDir;
 @property(readwrite,copy) NSArray * packFiles;
+
+- (NSArray*)loadPackFilesWithError:(NSError**)outError;
 @end
 /*! \endcond */
 
@@ -23,8 +25,39 @@
 {
     if(self = [super init])
     {
+        NSError * error;
         self.packsDir = [root stringByAppendingPathComponent:@"objects/pack"];
+        self.packFiles = [self loadPackFilesWithError:&error];
     }
     return self;
+}
+- (NSArray*)loadPackFilesWithError:(NSError**)outError
+{
+    NSError * error;
+    GITPackFile * pack;
+    NSMutableArray * packs;
+    NSFileManager * fm = [NSFileManager defaultManager];
+    NSArray * files    = [fm contentsOfDirectoryAtPath:dir error:&error];
+
+    if (files)
+    {
+        // Should only be pack & idx files, so div(2) should be about right
+        packs = [NSMutableArray arrayWithCapacity:[files count] / 2];
+        for (NSString * file in files)
+        {
+            if ([[file pathExtension] isEqualToString:@"pack"])
+            {
+                pack = [[GITPackFile alloc] initWithPath:[dir stringByAppendingPathComponent:file]]; // retain?
+                [packs addObject:pack];
+            }
+        }
+    }
+    else
+    {
+        outError = error; // The simple way, if we want to add more later we can :D
+        packs = nil;
+    }
+
+    return packs;
 }
 @end
