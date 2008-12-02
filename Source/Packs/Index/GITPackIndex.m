70d0052f322ff7f9252f5858997e8fef9e1897c2
Geoff Garside <geoff@geoffgarside.co.uk>
2008-12-02 01:19:32 +0000

    Remove some duplication of code by making checksumString methods derive from the checksum methods

--- Source/Packs/Index/GITPackIndex.m
+++ Source/Packs/Index/GITPackIndex.m
@@ -7,6 +7,7 @@
 //
 
 #import "GITPackIndex.h"
+#import "GITUtilityBelt.h"
 
 @implementation GITPackIndex
 #pragma mark -
@@ -57,13 +58,11 @@
 }
 - (NSString*)checksumString
 {
-    [self doesNotRecognizeSelector: _cmd];
-    return nil;
+    return unpackSHA1FromData([self checksum]);
 }
 - (NSString*)packChecksumString
 {
-    [self doesNotRecognizeSelector: _cmd];
-    return nil;
+    return unpackSHA1FromData([self packChecksum]);
 }
 - (BOOL)verifyChecksum
 {
