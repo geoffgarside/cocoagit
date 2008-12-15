9897d44871a9ceedc0e9cde0267faba8cef48e3d
Brian Chapados <chapados@sciencegeeks.org>
2008-12-14 18:03:54 -0800

    Refactored GITPackStore#loadObject... method to use GITError

--- Source/Stores/GITPackStore.m
+++ Source/Stores/GITPackStore.m
@@ -40,21 +40,20 @@
     NSData * objectData = nil;
 
     // Check the cached lastReadPack first
-    if (lastReadPack != nil)
+    if (lastReadPack != nil) {
         objectData = [self.lastReadPack dataForObjectWithSha1:sha1];
-    if (objectData) return objectData;
-
-    for (GITPackFile * pack in self.packFiles)
-    {
-        if (pack != self.lastReadPack)
-        {
-            objectData = [pack dataForObjectWithSha1:sha1];
-            if (objectData)
-            {
-                self.lastReadPack = pack;
-                return objectData;
-            }
-        }
+		if (objectData) return objectData;
+	}
+		
+    for (GITPackFile * pack in self.packFiles) {
+        if (pack == self.lastReadPack)
+			continue;
+		
+		objectData = [pack dataForObjectWithSha1:sha1];
+		if (objectData)	{
+			self.lastReadPack = pack;
+			return objectData;
+		}
     }
 
     return nil;
@@ -87,55 +86,38 @@
                       type:(GITObjectType*)type error:(NSError**)error
 {
     NSError * undError;
-    NSString * errorDescription;
-    NSDictionary * errorUserInfo;
 
-    // Check the cached lastReadPack first
-    if (lastReadPack != nil)
-    {
-        if ([self.lastReadPack loadObjectWithSha1:sha1 intoData:data type:type error:&undError])
-        {
-            return YES;
-        }
-        else if ([undError code] != GITErrorObjectNotFound && error != NULL)
-        {
-            errorUserInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                             [undError localizedDescription], NSLocalizedDescriptionKey,
-                             undError, NSUnderlyingErrorKey, nil];
-            *error = [NSError errorWithDomain:GITErrorDomain code:[undError code] userInfo:errorUserInfo];
-            return NO;
-        }
-    }
-
-    for (GITPackFile * pack in self.packFiles)
-    {
-        if (pack != self.lastReadPack)
-        {
-            if ([pack loadObjectWithSha1:sha1 intoData:data type:type error:&undError])
-            {
-                self.lastReadPack = pack;
-                return YES;
-            }
-            else if ([undError code] != GITErrorObjectNotFound && error != NULL)
-            {
-                errorUserInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-                                 [undError localizedDescription], NSLocalizedDescriptionKey,
-                                 undError, NSUnderlyingErrorKey, nil];
-                *error = [NSError errorWithDomain:GITErrorDomain code:[undError code] userInfo:errorUserInfo];
-                return NO;
-            }
-        }
-    }
-
+	if (lastReadPack != nil) {
+		if ([self.lastReadPack loadObjectWithSha1:sha1 intoData:data type:type error:&undError])
+			return YES;
+		if ([undError code] != GITErrorObjectNotFound) {
+			if (error != NULL)
+				*error = undError;
+			return NO;
+		}
+	}
+	
+	for (GITPackFile * pack in self.packFiles) {
+        if (pack == self.lastReadPack)
+			continue;
+		
+		if ([pack loadObjectWithSha1:sha1 intoData:data type:type error:&undError]) {
+			self.lastReadPack = pack;
+			return YES;
+		}
+		
+		if ([undError code] != GITErrorObjectNotFound) {
+			if (error != NULL)
+				*error = undError;
+			return NO;
+		}
+	}
+	
     // If we've made it this far then the object can't be found
-    if (error != NULL)
-    {
-        // no other error has been detected yet, so make our NotFound error
-        errorDescription = [NSString stringWithFormat:NSLocalizedString(@"Object %@ not found", @"GITErrorObjectNotFound"), sha1];
-        errorUserInfo = [NSDictionary dictionaryWithObject:errorDescription forKey:NSLocalizedDescriptionKey];
-        *error = [NSError errorWithDomain:GITErrorDomain code:GITErrorObjectNotFound userInfo:errorUserInfo];
-    }
-
+    // no other error has been detected yet, so make our NotFound error
+	NSString *errorDescription = [NSString stringWithFormat:NSLocalizedString(@"Object %@ not found", @"GITErrorObjectNotFound"), sha1];
+	GITError(error, GITErrorObjectNotFound, errorDescription);
+	
     return NO;
 }
 @end
