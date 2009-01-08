0dfe575dfb3cd9d8d8c411a4221a678c47237357
Geoff Garside <geoff@geoffgarside.co.uk>
2009-01-08 17:42:33 +0000

    Change NSData+Hashing to use CommonCrypto library from libSystem.
    Because libSystem with CommonCrypto is available on both Mac OS and
    the iPhone SDK this Crypto library will be more useful to port the
    project for use on an iPhone.

--- Source/Categories/NSData+Hashing.m
+++ Source/Categories/NSData+Hashing.m
@@ -8,40 +8,39 @@
 //  Methods extracted from source given at
 //  http://www.cocoadev.com/index.pl?NSDataCategory
 //
+//  The original NSDataCategory used OpenSSL for the
+//  hashing. This has been switched to CommonCrypto
+//  due to its availability on both iPhone and Mac OS.
+//
 
 #import "NSData+Hashing.h"
-#include <openssl/sha.h>
+#include <CommonCrypto/CommonDigest.h>
 
 @implementation NSData (Hashing)
 
 #pragma mark -
 #pragma mark Hashing macros
 #define HEComputeDigest(method)                                         \
-    method##_CTX ctx;                                                   \
-    unsigned char digest[method##_DIGEST_LENGTH];                       \
-    method##_Init(&ctx);                                                \
-    method##_Update(&ctx, [self bytes], [self length]);                 \
-    method##_Final(digest, &ctx);
+    CC_##method##_CTX ctx;                                              \
+    unsigned char digest[CC_##method##_DIGEST_LENGTH];                  \
+    CC_##method##_Init(&ctx);                                           \
+    CC_##method##_Update(&ctx, [self bytes], [self length]);            \
+    CC_##method##_Final(digest, &ctx);
 
 #define HEComputeDigestNSData(method)                                   \
     HEComputeDigest(method)                                             \
-    return [NSData dataWithBytes:digest length:method##_DIGEST_LENGTH];
+    return [NSData dataWithBytes:digest length:CC_##method##_DIGEST_LENGTH];
 
 #define HEComputeDigestNSString(method)                                 \
     static char __HEHexDigits[] = "0123456789abcdef";                   \
-    unsigned char digestString[2*method##_DIGEST_LENGTH];               \
+    unsigned char digestString[2*CC_##method##_DIGEST_LENGTH];          \
     unsigned int i;                                                     \
     HEComputeDigest(method)                                             \
-    for(i=0; i<method##_DIGEST_LENGTH; i++) {                           \
+    for(i=0; i<CC_##method##_DIGEST_LENGTH; i++) {                      \
         digestString[2*i]   = __HEHexDigits[digest[i] >> 4];            \
         digestString[2*i+1] = __HEHexDigits[digest[i] & 0x0f];          \
     }                                                                   \
-    return [NSString stringWithCString:(char *)digestString length:2*method##_DIGEST_LENGTH];
-
-#pragma mark -
-#pragma mark SHA1 Re-mappings
-#define SHA1_CTX				SHA_CTX
-#define SHA1_DIGEST_LENGTH		SHA_DIGEST_LENGTH
+    return [NSString stringWithCString:(char *)digestString length:2*CC_##method##_DIGEST_LENGTH];
 
 #pragma mark -
 #pragma mark SHA1 Hashing routines
