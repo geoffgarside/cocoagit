28cb6479d88977497fb5666fea21115bd3aed2a2
Brian Chapados <chapados@sciencegeeks.org>
2009-02-12 11:51:48 -0800

    Add SSHSession/SSHChannel classes: simple Obj-C interface to libssh2 for client connections.
    SSHSession handles user authentication, starting an ssh session, and provides
    a way to open an ssh channel for communication. Currently, password and public-key
    auth methods are supported, but I haven't provided a way to interactively supply
    a password yet.  Test it with public-key password-less logins for now.
    
    SSHChannel handles I/O for a session.
    
    We only support 'exec'ing commands for now.  Shell support should also be easy,
    but can't always be used for doing git+ssh anyway.  For remote repository setups like github,
    servers are typically configured to have 1 user (git), and the shell for that
    user is set to 'git-shell', which only allows the client to exec
    'git-upload-pack' or 'git-receive-pack' to initiate a fetch or push
    over ssh.  Since this is the most restrictive case, these same methods
    will work as long as the remote user can login and git is installed on
    the remote system.
    
    The I/O interface is intentionally designed to be identical to BufferedSocket.
    This should make it easy to create an SSHTransport class that uses SSH+git.
    
    NOTE: this changeset also re-instates pointer sign comparisons for the SSH target
    since those issues were fixed with the libssh2 1.0 upgrade.

---
+++ Source/Utils/SSHChannel.h
@@ -0,0 +1,38 @@
+//
+//  SSHChannel.h
+//  SSHSession
+//
+//  Created by Brian Chapados on 2/6/09.
+//  Copyright 2009 Brian Chapados. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "libssh2.h"
+
+extern const NSUInteger SSHChannelDefaultReadBufferSize;
+
+@interface SSHChannel : NSObject {
+    LIBSSH2_CHANNEL *channel;
+        
+    CFIndex readBufferSize;
+    void *readBuffer;
+    
+    NSMutableData *buffer;
+    NSError **channelError;
+}
+
+- (id) initWithSession:(LIBSSH2_SESSION *)sshSession;
+- (id) initWithChannel:(LIBSSH2_CHANNEL *)sshChannel;
+//- (void) openShell;
+
+- (BOOL) execCommand:(NSString *)command;
+- (NSMutableData *) readData:(CFIndex)n;
+- (NSMutableData *) readDataUpToData:(NSData *)d;
+- (NSMutableData *) readDataUpToString:(NSString *)s;
+- (void) writeData:(NSData *)data;
+- (void) sendEOF; // send channel EOF
+- (void) close;
+
+// error accessor
+- (NSError *)channelError;
+@end
---
+++ Source/Utils/SSHError.m
@@ -0,0 +1,40 @@
+//
+//  SSHError.m
+//  SSHSession
+//
+//  Created by Brian Chapados on 1/29/09.
+//  Copyright 2009 Brian Chapados. All rights reserved.
+//
+
+
+#import "SSHError.h"
+
+
+#define __ssh_error_domain(dom, str) NSString * const dom = str
+#define __ssh_error(code, val) struct error_code code = { val, @#code }
+
+__ssh_error_domain(SSHErrorDomain, @"org.sciencegeeks.SSHSession.ErrorDomain");
+
+__ssh_error(SSHErrorLookup, 4915);
+__ssh_error(SSHErrorAuth, 4916);
+__ssh_error(SSHErrorNoSession, 4917);
+__ssh_error(SSHErrorSession, 4918);
+__ssh_error(SSHErrorChannel, 4919);
+__ssh_error(SSHErrorSocketOpen, 4920);
+__ssh_error(SSHErrorSocketConnection, 4921);
+
+
+// generates an error message based on the last error reported by libssh2, preceded by a more general general description
+NSString *libssh2ErrorDescription(LIBSSH2_SESSION *session, NSString *description)
+{
+    char *ssh2_error;
+    int ssh2_code = libssh2_session_last_error(session, &ssh2_error, NULL, 0);
+    
+    NSString *errorDescription;
+    if (strlen(ssh2_error) == 0) {
+        errorDescription = [NSString stringWithFormat:@"%@ ([LIBSSH2 Error %d]", description, ssh2_code];
+    } else {
+        errorDescription = [NSString stringWithFormat:@"%@ ([LIBSSH2 Error %d]: %s)", description, ssh2_code,ssh2_error];
+    }
+    return errorDescription;
+}
