73bb125904ec2c365643df3d7c21680a3275de21
Brian Chapados <chapados@sciencegeeks.org>
2009-02-11 18:21:22 -0800

    Add GITFetchProcess class to manage the fetch process - currently just an outline.
    I'm not quite sure how to structure everything.  The main things
    we need to do are fetch and pull, so I thought maybe they could
    have their own manager classes, rather than coupling them
    to a specific client or server implementation.

---
+++ Source/Network/GITFetchProcess.m
@@ -0,0 +1,25 @@
+//
+//  GITFetchProcess.m
+//  CocoaGit
+//
+//  Created by chapbr on 2/10/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "GITFetchProcess.h"
+
+
+
+@implementation GITFetchProcess
+
+// -transport startFetch
+// (remote: sends refs + flush)
+// readRefs
+// sendNeeds/Wants?
+// (remote: recieve_needs)
+// (remote: get_common_commits)
+// (remote: send_pack_file + flush)
+// receivePackfile
+// write objects to disk
+
+@end
