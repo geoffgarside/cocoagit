a41add1fbf4c8c7cb55baeb74943fd8bea6e1193
Brian Chapados <chapados@sciencegeeks.org>
2009-02-25 17:35:33 -0800

    Cleanup and fix a bugs in Delta-patching code.
    There were a few places where cp_off was being set instead of cp_size.
    I also standardized on the use of position++ to track the current position
    in order to make the code easier to follow.

--- Source/Categories/NSData+Patching.m
+++ Source/Categories/NSData+Patching.m
@@ -11,22 +11,20 @@
 
 @implementation NSData (Patching)
 
-- (void)patchDeltaHeader:(NSData*)deltaData size:(unsigned long*)size position:(unsigned long*)position
+- (void)patchDeltaHeader:(NSData*)deltaData size:(unsigned long*)size position:(unsigned long*)position;
 {
     int shift = 0;
     unsigned char c;
     *size = 0;
     
     do {
-        [deltaData getBytes:&c range:NSMakeRange(*position, 1)];
-        *position += 1;
-        
+        [deltaData getBytes:&c range:NSMakeRange((*position)++, 1)];        
         *size |= ((c & 0x7f) << shift);
         shift += 7;
     } while ( (c & 0x80) != 0 );
 }
 
-- (NSData*)dataByPatchingWithDelta:(NSData*)deltaData
+- (NSData*)dataByPatchingWithDelta:(NSData*)deltaData;
 {
     unsigned long sourceSize, destSize, position = 0;
     unsigned long cp_off, cp_size;
@@ -35,74 +33,61 @@
     int bufLength = 1000;
     
     [self patchDeltaHeader:deltaData size:&sourceSize position:&position];
+    destSize = 0;
     [self patchDeltaHeader:deltaData size:&destSize position:&position];
     
-    NSMutableData *buffer = [[NSMutableData alloc] initWithCapacity:bufLength];
     NSMutableData *destination = [NSMutableData dataWithCapacity:destSize];
-    
-    while (position < [deltaData length]) {
-        [deltaData getBytes:&c range:NSMakeRange(position, 1)];
         
-        ++position;
+    while (position < [deltaData length]) {
+        [deltaData getBytes:&c range:NSMakeRange(position++, 1)];
+        
         if ( (c & 0x80) != 0 ) {
-            --position;
-            cp_off = cp_size = 0;
             
+            cp_off = 0;
             if ( 0 != (c & 0x01) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
                 cp_off = d;
             }
             if ( 0 != (c & 0x02) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
                 cp_off |= d << 8;
             }
             if ( 0 != (c & 0x04) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
                 cp_off |= d << 16;
             }
             if ( 0 != (c & 0x08) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
                 cp_off |= d << 24;
             }
+            
+            cp_size = 0;
             if ( 0 != (c & 0x10) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
-                cp_off |= d;
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
+                cp_size |= d;
             }
             if ( 0 != (c & 0x20) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
-                cp_off |= d << 8;
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
+                cp_size |= d << 8;
             }
             if ( 0 != (c & 0x40) ) {
-                [deltaData getBytes:&d range:NSMakeRange(++position, 1)];
-                cp_off |= d << 16;
+                [deltaData getBytes:&d range:NSMakeRange(position++, 1)];
+                cp_size |= d << 16;
             }
-            
             if (cp_size == 0)
                 cp_size = 0x10000;
             
-            ++position;
-            
-            if (cp_size > bufLength) {
-                bufLength = cp_size + 1;
-                [buffer setLength:bufLength];
-            }
-            
-            [self getBytes:[buffer mutableBytes] range:NSMakeRange(cp_off, cp_size)];
-            [destination appendBytes:[buffer bytes] length:cp_size];
+            [destination appendData:[self subdataWithRange:NSMakeRange(cp_off, cp_size)]];
         } else if ( c != 0 ) {
-            if ( c > destSize )
-                break;
-            
-            [deltaData getBytes:[buffer mutableBytes] range:NSMakeRange(position, c)];
-            [destination appendBytes:[buffer bytes] length:c];
+            [destination appendData:[deltaData subdataWithRange:NSMakeRange(position, c)]];
             position += c;
-            destSize -= c;
         } else {
+            [NSException raise:@"InvalidDeltaDataException" format:@"Delta data is invalid. Your packfile might be corrupt."]; 
+            NSLog(@"invalid delta data");
             // invalid delta data
         }
     }
     
-    [buffer release];
     return [[destination copy] autorelease];
 }
 
