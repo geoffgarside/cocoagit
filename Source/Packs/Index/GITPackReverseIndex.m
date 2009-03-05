0e04398e6d8e036a5436d38f26cba5c8814e6568
Brian Chapados <chapados@sciencegeeks.org>
2009-03-04 22:10:57 -0800

    Add support for extended (64-bit) offsets in revindex.
    Added anohter array to the reverse index for indexing and
    searching entries in the extended offset table.

--- Source/Packs/Index/GITPackReverseIndex.m
+++ Source/Packs/Index/GITPackReverseIndex.m
@@ -79,6 +79,8 @@
 @synthesize index;
 @synthesize offsets;
 @synthesize indexMap;
+@synthesize offsets64;
+@synthesize indexMap64;
 
 /*
 + (id) reverseIndexWithIndex:(GITPackIndex *)packIndex;
@@ -114,49 +116,90 @@
     NSAutoreleasePool *pool = [NSAutoreleasePool new];
     NSUInteger objectCount = [[self index] numberOfObjects];    
     NSMutableArray *off32 = [[NSMutableArray alloc] initWithCapacity:objectCount];
+    NSMutableArray *off64 = [NSMutableArray new];
     
-    NSUInteger i;    
+    NSUInteger i;
     for(i = 0; i < objectCount; i++) {
-        NSUInteger packOffset = [index packOffsetWithIndex:i];
-        [off32 addObject:[NSNumber numberWithUnsignedInt:packOffset]];
+        off_t packOffset = [index packOffsetWithIndex:i];
+        if (packOffset > UINT32_MAX) {
+            [off64 addObject:[NSNumber numberWithUnsignedLongLong:packOffset]];
+        } else {
+            [off32 addObject:[NSNumber numberWithUnsignedLong:packOffset]];
+        }
     }
         
-    NSArray *off32Sorted = [off32 sortedArrayUsingSelector:@selector(compare:)];
-    [self setOffsets:off32Sorted];
-    
-    NSMutableArray *index32 = [[NSMutableArray alloc] initWithCapacity:objectCount];
-    for(i = 0; i < objectCount; i++) [index32 addObject:[NSNull null]];
-    for(i = 0; i < objectCount; i++) {
+    [self setOffsets:[off32 sortedArrayUsingSelector:@selector(compare:)]];
+    [self setOffsets64:[off64 sortedArrayUsingSelector:@selector(compare:)]];
+
+    NSMutableArray *index32 = [off32 mutableCopy];
+    for(i = 0; i < [offsets count]; i++) {
         NSUInteger offsetIndex = [offsets bsIndexOfNumber:[off32 objectAtIndex:i]];
         [index32 replaceObjectAtIndex:offsetIndex withObject:[NSNumber numberWithUnsignedInt:i]];
     }
-    
     [self setIndexMap:[NSArray arrayWithArray:index32]];
     [off32 release];
     [index32 release];
+    
+    NSMutableArray *index64 = [off64 mutableCopy];
+    for(i = 0; i < [offsets64 count]; i++) {
+        NSUInteger offsetIndex = [offsets64 bsIndexOfNumber:[off64 objectAtIndex:i]];
+        [index64 replaceObjectAtIndex:offsetIndex withObject:[NSNumber numberWithUnsignedInt:i]];
+    }
+    [self setIndexMap64:[NSArray arrayWithArray:index64]];
+    [off64 release];
+    [index64 release];
+    
+    //NSLog(@"GITPackReverseIndex -buildReverseIndex: (%d) 32bit offsets, (%d) 64bit offsets)", [offsets count], [offsets64 count]);
+        
     [pool release];
     
     return YES;
 }
 
-- (NSUInteger) indexWithOffset:(NSUInteger)offset;
+- (NSUInteger) indexWithOffset64:(off_t)offset;
 {
-    NSUInteger i = [offsets bsIndexOfNumber:[NSNumber numberWithUnsignedInt:offset]];
+    NSUInteger i = [offsets64 bsIndexOfNumber:[NSNumber numberWithUnsignedLongLong:offset]];
+    if (i == NSNotFound)
+        return NSNotFound;
+    return [[indexMap64 objectAtIndex:i] unsignedIntValue];
+}
+
+- (NSUInteger) indexWithOffset:(off_t)offset;
+{
+    if (offset > UINT32_MAX)
+        return [self indexWithOffset64:offset];
+    NSUInteger i = [offsets bsIndexOfNumber:[NSNumber numberWithUnsignedLong:offset]];
+    if (i == NSNotFound)
+        return NSNotFound;
     return [[indexMap objectAtIndex:i] unsignedIntValue];
+}
+
+- (off_t) nextOffsetWithOffset64:(off_t)thisOffset;
+{
+    NSNumber *offsetNumber = [NSNumber numberWithUnsignedLongLong:thisOffset];
+    NSUInteger i = [offsets64 bsIndexOfNumber:offsetNumber];
+    if (i == NSNotFound)
+        return NSNotFound;
+    if (i+1 == [offsets64 count])
+        return -1;
+    NSNumber *nextOffset = [offsets64 objectAtIndex:i+1];
+    return (off_t)[nextOffset unsignedLongLongValue];
 }
 
 // return the next offset after object at offset: thisOffset
 //   return NSNotFound if thisOffset isn't found
 //   return -1 if thisOffset is the last offset
-- (NSUInteger) nextOffsetWithOffset:(NSUInteger)thisOffset;
+- (off_t) nextOffsetWithOffset:(off_t)thisOffset;
 {
-    NSUInteger i = [offsets bsIndexOfNumber:[NSNumber numberWithUnsignedInt:thisOffset]];
+    if (thisOffset > UINT32_MAX)
+        return [self nextOffsetWithOffset64:thisOffset];
+    NSUInteger i = [offsets bsIndexOfNumber:[NSNumber numberWithUnsignedLong:thisOffset]];
     if (i == NSNotFound)
         return NSNotFound;
     if (i+1 == [offsets count])
         return -1;
     NSNumber *nextOffset = [offsets objectAtIndex:i+1];
-    return (NSUInteger)[nextOffset unsignedIntValue];
+    return (off_t)[nextOffset unsignedLongValue];
 }
 
 @end
